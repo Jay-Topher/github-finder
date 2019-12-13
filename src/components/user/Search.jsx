fcbabdc775520a2915eeeb23a53da8ac6eaf1437
Jones Ogolo <jay.topher11@gmail.com>
2019-12-13 13:38:33 +0100

    added proptypes to my search component

--- src/components/user/Search.jsx
+++ src/components/user/Search.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class Search extends Component {
   state = {
     text: ""
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
   };
 
   onChange = e => {
@@ -12,11 +20,16 @@ export class Search extends Component {
 
   onSubmit = e => {
     e.preventDefault();
-    this.props.searchUsers(this.state.text);
-    this.setState({text: ''})
+    if (this.state.text === "") {
+      this.props.setAlert("Please enter something", "light");
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: "" });
+    }
   };
 
   render() {
+    const { clearUsers, showClear } = this.props;
     return (
       <div>
         <form className="form" onSubmit={this.onSubmit}>
@@ -33,6 +46,11 @@ export class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
