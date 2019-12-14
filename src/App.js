65b3dbd9beaff4b32b770bd28f7ce804bfa41358
Jones Ogolo <jay.topher11@gmail.com>
2019-12-14 03:56:03 +0100

    created a function to get user repos

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ class App extends Component {
     users: [],
     user: {},
     loading: false,
-    alert: null
+    alert: null,
+    repos: []
   };
 
   // async componentDidMount() {
@@ -57,8 +58,19 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  // get user repos
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({repos: res.data, loading: false})
+  };
+
   render() {
-    const { users, loading, user } = this.state;
+    const { users, loading, user, repos } = this.state;
     return (
       <Router>
         <div>
@@ -89,7 +101,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
