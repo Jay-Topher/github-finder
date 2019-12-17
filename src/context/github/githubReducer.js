a6bc137ec6224dc80bd4c204f651e353ea823204
Jones Ogolo <jay.topher11@gmail.com>
2019-12-17 18:48:43 +0100

    added actions get user and clear users to the github reducer

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -9,20 +9,34 @@ import {
 } from "../types";
 
 export default (state, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case SEARCH_USERS:
       return {
         ...state,
         users: action.payload,
         loading: false
-      }
+      };
+
+    case CLEAR_USERS:
+      return {
+        ...state,
+        users: [],
+        loading: false
+      };
     case SET_LOADING:
       return {
         ...state,
-        loading: true,
-      }
+        loading: true
+      };
+
+    case GET_USER:
+      return {
+        ...state,
+        user: action.payload,
+        loading: false
+      };
 
     default:
       return state;
   }
-}
+};
