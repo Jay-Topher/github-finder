2296c3cbb1d0bd5a1271302ab9adcb14e2725b6d
Jones Ogolo <jay.topher11@gmail.com>
2019-12-18 21:15:01 +0100

    took out repos and get user repos from props

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useContext } from "react";
-import PropTypes from "prop-types";
 import Spinner from "../layout/Spinner";
 import Repos from "../repos/Repos";
 import { Link } from "react-router-dom";
 import GithubContext from "../../context/github/githubContext";
 
-const User = ({ repos, getUserRepos, match }) => {
+const User = ({ match }) => {
   const githubContext = useContext(GithubContext)
-  const { getUser, loading, user } = githubContext;
+  const { getUser, loading, user, getUserRepos, repos } = githubContext;
   useEffect(() => {
     getUser(match.params.login);
     getUserRepos(match.params.login);
@@ -101,9 +100,5 @@ const User = ({ repos, getUserRepos, match }) => {
   );
 };
 
-User.propTypes = {
-  repos: PropTypes.array.isRequired,
-  getUserRepos: PropTypes.func.isRequired
-};
 
 export default User;
